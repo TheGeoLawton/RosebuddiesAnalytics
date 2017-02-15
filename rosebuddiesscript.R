a7b8e066a71ae67b59ba76ac01422f154fbd9ed7
thegeolawton <geofry.m.lawton@gmail.com>
2017-02-15 10:49:05 -0500

    Added some summary reports.

--- rosebuddiesscript.R
+++ rosebuddiesscript.R
@@ -1,3 +1,6 @@
 fbk = fbOAuth(501083100089647,"0aaa4cd08c7617284e2ef6cf7ef39b73")
 result <- getPost(669202253265873,fbk)
 comments <- result$comments
+
+# See top 10 users by number of posts
+summary1 <- comments %>% group_by(from_name) %>% summarize(count = n()) %>% arrange(desc(count)) %>% top_n(10,count)
