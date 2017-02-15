adf9d4e1440061139b9e1f40a751460a330572ee
thegeolawton <geofry.m.lawton@gmail.com>
2017-02-15 12:38:15 -0500

    Added associations

--- rosebuddiesscript.R
+++ rosebuddiesscript.R
@@ -6,7 +6,10 @@ fbk = fbOAuth(501083100089647,"0aaa4cd08c7617284e2ef6cf7ef39b73")
 result <- getPost(669202253265873,fbk) #default limits to 500 comments
 comments <- result$comments
 
-# See top 10 users by number of posts
+# ------------------------------------------------------Find associations to a given term
+findAssocs(dm,"nick",.25)
+
+# ----------------------------------------------------See top 10 users by number of posts
 summary1 <- comments %>% group_by(from_name) %>% summarize(count = n()) %>% arrange(desc(count)) %>% top_n(10,count)
 
 #----------------------------------------------Generate word cloud with wordcloud library
